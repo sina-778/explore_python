a2d5b00a19cd7821f5d2b4c32a4eafd48d97fbad
sina-778 <isina413.is@gmail.com>
2025-11-01 23:04:29 +0100

    Add fruits list and print statement

--- python_basic.py
+++ python_basic.py
@@ -1,2 +1,8 @@
 fruits = ['apple', 'pear', 'orange', 'banana', 'apple']
 print(fruits)
+
+print(len(fruits))
+print(fruits.count('apple'))
+print(fruits.index('orange'))
+fruits.sort()
+print(fruits)
